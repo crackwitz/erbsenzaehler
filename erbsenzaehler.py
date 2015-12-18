a68bd3363be4688f969d36efef8ca9bdc69b1e71
Christoph Rackwitz <christoph.rackwitz@gmail.com>
2015-12-18 21:57:08 +0100

    maybe not better, but different

--- erbsenzaehler.py
+++ erbsenzaehler.py
@@ -2,6 +2,8 @@
 from __future__ import division
 import sys
 import serial
+import math
+import pprint; pp = pprint.pprint
 
 
 def argmin(arg, key=None):
@@ -18,34 +20,60 @@ def argmin(arg, key=None):
 	else:
 		return min(xrange(len(arg)), key=keyfn)
 
+def mean(seq):
+	lst = list(seq)
+	if not lst:
+		return None
+	else:
+		return sum(lst) / len(lst)
 
-class SettlingValue(object):
-	def __init__(self, range=None, threshold=None, history=5):
+def ValueStream(fd, scale=None):
+	while True:
+		try:
+			line = fd.next()
+		except StopIteration:
+			continue
+
+		try:
+			value = int(line)
+			if scale is not None:
+				value *= scale
+			yield value
+
+
+		except ValueError, e:
+			continue
+
+class StableValue(object):
+	def __init__(self, range=None, threshold=None, history=4):
+		self.history = history
 		self.threshold = threshold
 		self.range = range
 		assert not (range is None and threshold is None)
-		self.values = (None,) * history
+		self.clear()
+
+	def clear(self):
+		self.values = (None,) * self.history
 	
 	def update(self, newval):
 		self.values = self.values[1:] + (newval,)
+		return self.stable
 
-	def is_settled(self):
-		if not all(x is not None for x in self.values):
+	def is_stable(self):
+		if any(x is None for x in self.values):
 			return False
 
-		#nn = [x for x in self.values if x is not None]
-		nn = self.values
-		u = min(nn)
-		v = max(nn)
+		u = min(self.values)
+		v = max(self.values)
 		if self.range:
-			return (v-u) < self.range
+			return (v-u) <= self.range
 		elif self.threshold:
 			return (v-u) < self.threshold * (u+v) * 0.5
 
-	settled = property(is_settled)
+	stable = property(is_stable)
 
 	def __float__(self):
-		return float(self.values[-1] or 0.0)
+		return mean(v for v in self.values if v is not None) or 0.0
 
 class AverageValue(object):
 	def __init__(self, alpha=0.05, fmt="f", init=None):
@@ -82,165 +110,125 @@ class AverageValue(object):
 
 		return res
 
-class ValueStream(object):
-	def __init__(self, fd):
-		self.fd = fd
+def DeltaStream(input, tare=0.1):
+	zero = AverageValue()
+	increment = StableValue(range=tare)
 
-	def __iter__(self):
-		return self
+	# init
+	for newval in input:
+		if increment.update(newval):
+			zero.update(float(increment))
+			increment.clear()
+			break
 
-	def next(self):
-		while True:
-			line = self.fd.readline()
+	for newval in input:
+		delta = newval - float(zero)
 
-			try:
-				return int(line)
-			except ValueError, e:
-				continue
+		if abs(delta) <= tare:
+			zero.update(newval)
+			increment.clear()
+			continue
 
-class MixedBag(object):
-	# this should be doing a mixture of gaussians
-	# this should keep track of all increments to recompute sigmas
-	# this should recluster on demand or when needed
+		if not increment.update(delta):
+			# not stable
+			continue
 
-	# TODO: need a new class to emit increments and track/calibrate otherwise
+		# stable. reset zero and return averaged delta.
+		zero.clear()
+		zero.update(newval)
+		yield float(increment)
 
-	def __init__(self, new_abs=0.1, new_rel=0.1):
-		# exactly one candidate category should score within relative range for a count
-		# for best candidate, update / round(count) should match exactly one weight, its own
-		# thresholds to trigger a new category (both need to be overstepped)
-		self.th_abs = new_abs
-		self.th_rel = new_rel
+class Cluster(object):
+	# http://www.johndcook.com/blog/standard_deviation/
+	def __init__(self, mdev0=None, values=()):
+		self.count = 0
+		self.mean = 0.0
+		self.mdev = mdev0 or 0.0
+		for v in values:
+			self.add(v)
 
-		self.clear()
+	def add(self, value, count=1):
+		dev = value - self.mean
 
-	def clear(self):
-		self.kinds = 0
-		self.counts = []
-		self.weights = []
+		self.mean = (self.mean * self.count + value * count) / (self.count + count)
+		self.count += count
 
-	def add_kind(self, weight=None, count=0):
-		self.counts.append(count)
-		self.weights.append(AverageValue(init=weight, alpha=1, fmt="{0:+7.3f} g"))
-		self.kinds += 1
+		if self.count > 1:
+			self.mdev += (abs(dev) - self.mdev) * count / self.count
 
-	def remove_kind(self, kind):
-		del self.counts[kind]
-		del self.weights[kind]
-		self.kinds -= 1
+	def remove(self, count=1):
+		self.count -= count
 
-	def __iter__(self):
-		for k in xrange(self.kinds):
-			yield float(self.weights[k]), self.counts[k]
+	def __cmp__(self, other):
+		return (self.mean > other.mean) - (self.mean < other.mean)
 
-	def total(self, *keys):
-		if not keys:
-			return sum( w*c for k,(w,c) in enumerate(self))
+	def __repr__(self):
+		return "<Cluster {0:.2f} ~{1:.2f} n={2:d}>".format(self.mean, self.mdev, self.count)
+
+class Mixture(object):
+	def __init__(self, mdev0, th_merge=3):
+		self.mdev0 = mdev0
+		self.th_merge = th_merge
+		self.clusters = set()
+
+	def evaluate(self, value):
+		err = (lambda x:
+			abs(x - round(x)))
+		score = (lambda c:
+			err(abs(value) / c.mean) * c.mean / c.mdev)
+
+		scores = {
+			c: score(c)
+			for c in self.clusters
+			if (value > 0 and c.count >= 0) or (value < 0 and c.count > 0)
+		}
+
+		best = argmin(scores)
+		if best in scores:
+			count = int(round(abs(value) / best.mean))
 		else:
-			return sum( w*c for k,(w,c) in enumerate(self) if k in keys)
+			count = None
 
-	def update(self, new_total):
-		# check if it fits into exactly one category
-		# if not, could it be a new category?
-		# otherwise, do nothing
+		return best, count, scores.get(best, None)
 
+	def add(self, value):
+		(best, count, sigmas) = self.evaluate(value)
+		does_fit = (best is not None) and (sigmas <= self.th_merge)
 
-		# categorize, or init new category
-		old_total = self.total()
-		update = new_total - old_total
+		if value > 0:
+			# check if it fits anywhere
+			if does_fit:
+				best.add(value/count, count=count)
+			else:
+				self.clusters.add(Cluster(values=[value], mdev0=self.mdev0(value)))
 
-		# update all equally
-		if abs(update) < self.th_abs:
-			factor = new_total / old_total
-			for w in self.weights:
-				w.update(float(w) * factor)
+		elif value < 0:
+			if does_fit:
+				best.remove(count=count)
+				if best.count == 0:
+					self.clusters.remove(best)
+			else:
+				self.clusters = set()
 			return
 
-		multiples = [ update / float(w) for w in self.weights ]
-		rel_errors = [abs(m - round(m)) for m in multiples ] # fractional error to nearest multiple
-		abs_errors = [ e * float(w) for e,w in zip(rel_errors, self.weights) ] # how much that really is
 
-		if sum(e < self.th_rel for e in rel_errors) > 1:
-			# ambiguous. can't work with that.
-			return
-
-		# best (only) match in terms of being a full multiple
-		closest = argmin(abs_errors)
-		if closest is None:
-			self.add_kind(update, 1)
-			return
-
-		dcount = int(round(multiples[closest]))
-		error_rel = rel_errors[closest]
-		error_abs = abs_errors[closest]
-
-		if error_abs > self.th_abs:
-			if update > 0:
-				self.add_kind(update, 1)
-			return
-
-		if self.counts[closest] + dcount >= 0:
-			# compute what must be the remainder
-			kindsum = update + self.total(closest)
-			self.counts[closest] += dcount
-			if self.counts[closest] == 0:
-				self.remove_kind(closest)
-			elif self.counts[closest] > 0:
-				self.weights[closest].update(kindsum / self.counts[closest])
+	def __repr__(self):
+		return "<Mixture {0!r}>".format(sorted(self.clusters))
 
 
 if __name__ == '__main__':
-	scale = 720.0e-6 # calibration parameter
-
-	current = SettlingValue(range=0.1, history=4) # grams
-
-	zero = AverageValue(alpha=0.02, fmt=u"{0:9.1f}") # in counts
-	tarethreshold = 0.1 # grams
-
-	# usable weight, neq 0
-	weight = AverageValue(alpha=0.1, fmt=u"{0:+7.2f} g") # grams
-
-	bag = MixedBag()
-
-	# obsolete
-	singleweight = AverageValue(alpha=0.2, fmt="{0:+7.3f} g") # grams
-	count = 0
-
 	comport = sys.argv[1]
 
 	ser = serial.Serial(comport, baudrate=9600, timeout=0.2)
 	assert ser.isOpen()
 
-	for rawvalue in ValueStream(ser):
-		print u"raw: {0:8.0f}, zero {1:s}, current {2:+7.2f} g, weight {3:s} -> {4:s}".format(
-			rawvalue,
-			zero,
-			float(current),
-			weight,
-			", ".join("{1}x {0:.2f}".format(w,c) for w,c in bag)
-		)
+	scale = 720.0e-6 # calibration parameter
+	deltas = DeltaStream(ValueStream(ser, scale=scale), tare=0.1)
+	mixture = Mixture(mdev0=(lambda value: 0.02 * value), th_merge=3)
 
-		currentvalue = (rawvalue - float(zero)) * scale
-		current.update(currentvalue)
+	for increment in deltas:
+		print "{0:+6.2f}".format(increment)
+		#if increment > 0:
+		mixture.add(increment)
 
-		if not current.is_settled():
-			weight.clear()
-			continue
-
-		if not zero.is_valid() or abs(float(current)) < tarethreshold:
-			zero.update(rawvalue)
-			weight.clear()
-			bag.clear()
-			count = 0
-			continue
-
-		weight.update(float(current))
-
-		#if not weight.is_settled():
-		#	continue
-
-		if float(weight) < 0:
-			continue
-
-		bag.update(float(weight))
-
+		print mixture
